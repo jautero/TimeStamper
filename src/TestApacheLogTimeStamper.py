a3710e5ee0ca7f413213046fdcbd815b737e94d8
Juha Autero <jautero@iki.fi>
2013-11-17 14:09:58 +0200

    Added format to test

--- src/TestApacheLogTimeStamper.py
+++ src/TestApacheLogTimeStamper.py
@@ -2,10 +2,13 @@
 #
 # Parse gets for given URL and use them to generate timestamps.
 #
-import unittest
+import unittest, apachelog
 import ApacheLogTimeStamper
 
 class ApacheLogTimeStamperTest(unittest.TestCase):
+    testtarget="/testtarget"
+    testformat=apachelog.formats["extended"]
     def test_create(self):
-        stamper=ApacheLogTimeStamper.ApacheLogTimeStamper("/testtarget")
-        
+        stamper=ApacheLogTimeStamper.ApacheLogTimeStamper(self.testtarget,self.testformat)
+        self.assert_(stamper.gettarget==self.testtarget, 'stamper.gettarget is not %s: %s ' % (self.testtarget,stamper.gettarget))
+        self.assert_(stamper.format==self.testformat, 'stamper.format is not %s: %s' % (self.testformat,stamper.format))
